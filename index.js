504a976ef288e2c0bbef0ca410258f5986241ef7
Al Amin Eimon <eimonalamin@gmail.com>
2022-11-07 22:49:37 +0600

    Basic server setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+//mmiddle wares
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (res, req) => {
+  req.send("Incredible Trip server is running");
+});
+
+
+app.listen(port, () => {
+  console.log(`Incredible Trip Server running onporrt: ${port}`);
+});
