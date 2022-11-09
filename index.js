3981a568b199ffa4215f0dc55ea9d8b81cd63a14
Al Amin Eimon <eimonalamin@gmail.com>
2022-11-09 08:25:30 +0600

    data changed

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.juguzvf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+
+
+
+
 app.get("/", (res, req) => {
   req.send("Incredible Trip server is running");
 });
