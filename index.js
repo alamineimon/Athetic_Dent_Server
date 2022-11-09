d32b3e52b72450c81caba40ab9d79d3a96e7afe4
Al Amin Eimon <eimonalamin@gmail.com>
2022-11-09 20:41:47 +0600

    Review  route  set

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.juguzvf.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -17,6 +16,54 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+
+async function run() {
+  try {
+    const serviceCollection = client.db("athetic").collection("services");
+    const reviewsCollection = client.db("athetic").collection("review");
+
+    //all service data loaad
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+
+    // load a single data from mangodb 
+        app.get('/services/:id' , async(req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const services = await serviceCollection.findOne(query)
+            res.send(services);
+        });
+    app.post('/review', async(req, res)=>{
+            const review = req.body ;
+            const result = await reviewsCollection.insertOne(review);
+            res.send(result) 
+    });
+
+    app.get("/allreview", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewsCollection.find(query);
+      const review = await cursor.toArray();
+      console.log(review);
+      res.send(review);
+    });
+
+
+  }
+  finally{
+
+  }
+}
+ run().catch(err =>console.error(err))
 
 
 
