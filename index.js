b445a73300048e63fde0c4da2f4e67ed8961c325
Al Amin Eimon <eimonalamin@gmail.com>
2022-11-09 22:36:55 +0600

    deletee route  ok

--- index.js
+++ index.js
@@ -58,6 +58,15 @@ async function run() {
     });
 
 
+
+    app.delete("/allreview/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewsCollection.deleteOne(query);
+      res.send(result);
+    });
+
+
   }
   finally{
 
